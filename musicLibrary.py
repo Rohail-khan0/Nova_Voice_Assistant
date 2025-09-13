b99dbfc81cab353ad28c1b1f63cafd05f46bead7
Rohail Khan <uk6277643@gmail.com>
2025-09-13 18:21:19 +0500

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music= {
+  "wakawaka": "https://youtu.be/pRpeEdMmmQ0?feature=shared",
+  "wavinflag" : "https://youtu.be/WTJSt4wP2ME?feature=shared",
+  "magicintheair" : "https://youtu.be/BAkqJT_sMKQ?feature=shared",
+  "halaand": "https://youtu.be/cixr-7fMWwU?feature=shared",
+  "djbravo": "https://youtu.be/Y963o_1q71M?feature=shared"
+}
